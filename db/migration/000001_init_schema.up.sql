2d8337b032f48be60d82c3f29e5b8ca2485b3fc7
dhruv42 <dhruvpopat42@gmail.com>
2022-01-11 23:31:33 +0530

    Add users table, add test cases for entries schema, develop logic to transfer amount across accounts.

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -8,34 +8,34 @@ CREATE TABLE "accounts" (
 
 CREATE TABLE "entries" (
   "id" bigserial PRIMARY KEY,
-  "accound_id" bigint NOT NULL,
+  "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
   "id" bigserial PRIMARY KEY,
-  "from_accound_id" bigint NOT NULL,
-  "to_accound_id" bigint NOT NULL,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-ALTER TABLE "entries" ADD FOREIGN KEY ("accound_id") REFERENCES "accounts" ("id");
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_accound_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
-ALTER TABLE "transfers" ADD FOREIGN KEY ("to_accound_id") REFERENCES "accounts" ("id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
 
 CREATE INDEX ON "accounts" ("owner");
 
-CREATE INDEX ON "entries" ("accound_id");
+CREATE INDEX ON "entries" ("account_id");
 
-CREATE INDEX ON "transfers" ("from_accound_id");
+CREATE INDEX ON "transfers" ("from_account_id");
 
-CREATE INDEX ON "transfers" ("to_accound_id");
+CREATE INDEX ON "transfers" ("to_account_id");
 
-CREATE INDEX ON "transfers" ("from_accound_id", "to_accound_id");
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
 
 COMMENT ON COLUMN "entries"."amount" IS 'can be -ve or +ve';
 
--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -16,7 +16,7 @@ type Account struct {
 
 type Entry struct {
 	ID        int64 `json:"id"`
-	AccoundID int64 `json:"accoundID"`
+	AccountID int64 `json:"accountID"`
 	// can be -ve or +ve
 	Amount    int64     `json:"amount"`
 	CreatedAt time.Time `json:"createdAt"`
@@ -24,9 +24,18 @@ type Entry struct {
 
 type Transfer struct {
 	ID            int64 `json:"id"`
-	FromAccoundID int64 `json:"fromAccoundID"`
-	ToAccoundID   int64 `json:"toAccoundID"`
+	FromAccountID int64 `json:"fromAccountID"`
+	ToAccountID   int64 `json:"toAccountID"`
 	// must only be +ve
 	Amount    int64     `json:"amount"`
 	CreatedAt time.Time `json:"createdAt"`
+}
+
+type User struct {
+	Username          string    `json:"username"`
+	HashedPassword    string    `json:"hashedPassword"`
+	FullName          string    `json:"fullName"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"passwordChangedAt"`
+	CreatedAt         time.Time `json:"createdAt"`
 }
