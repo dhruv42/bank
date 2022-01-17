70bbb16bf4c0561a9534fc9f7bdea9876ac3e810
dhruv42 <dhruvpopat42@gmail.com>
2022-01-17 20:30:23 +0530

    Fix: account2 ID should be equal to toAccount ID

--- db/sqlc/store_test.go
+++ db/sqlc/store_test.go
@@ -42,7 +42,7 @@ func TestTransferTx(t *testing.T) {
 		transfer := result.Transfer
 		require.NotEmpty(t, transfer)
 		require.Equal(t, account1.ID, transfer.FromAccountID)
-		require.Equal(t, account1.ID, transfer.ToAccountID)
+		require.Equal(t, account2.ID, transfer.ToAccountID)
 		require.Equal(t, amount, transfer.Amount)
 		require.NotZero(t, transfer.ID)
 		require.NotZero(t, transfer.CreatedAt)
