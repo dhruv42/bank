44acb5434d81e0090f42ea886488e12a3c7c4d0a
dhruv42 <dhruvpopat42@gmail.com>
2022-01-29 19:45:26 +0530

    Develop transfer API, add custom validator.

--- api/server.go
+++ api/server.go
@@ -3,6 +3,8 @@ package api
 import (
 	db "github.com/dhruv42/bank/db/sqlc"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
@@ -14,10 +16,15 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.createAccount)
 	router.GET("/account/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.PATCH("/account/:id", server.updateAccount)
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
